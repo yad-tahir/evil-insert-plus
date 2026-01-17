f4b385ea323b9c9af2fdfa1f4c5d6abb19e3dd83
Dr. Yad Tahir <yad@ieee.org>
2026-01-17 21:43:45 +0300

    Whitespace fix

--- evil-insert-plus.el
+++ evil-insert-plus.el
@@ -23,33 +23,33 @@ IS-APPEND determines if the operation is an append or insert."
   ;; append logic maintains parity with change operation and handles numerous
   ;; edge cases correctly.
   (let ((p-before (point))
-		(inhibit-modification-hooks t)
-		;; Suppress macro and undos
-		(defining-kbd-macro nil)
-		(executing-kbd-macro t)
-		(buffer-undo-list nil))
-	(catch 'evil-insert-plus-quit
-	  (save-excursion
-		(atomic-change-group
-		  (let ((size-before (buffer-size)))
-			;; Perform deletion into the black hole register
-			(evil-delete beg end type ?_)
-			(let* ((displacement (- size-before (buffer-size)))
-				   (result (if (= p-before (point)) ;; Forward motion
-							   (if is-append (+ beg displacement) beg)
-							 ;; Backward motion
-							 (if is-append end (point)))))
-			  (throw 'evil-insert-plus-quit result))))))))
+        (inhibit-modification-hooks t)
+        ;; Suppress macro and undos
+        (defining-kbd-macro nil)
+        (executing-kbd-macro t)
+        (buffer-undo-list nil))
+    (catch 'evil-insert-plus-quit
+      (save-excursion
+        (atomic-change-group
+          (let ((size-before (buffer-size)))
+            ;; Perform deletion into the black hole register
+            (evil-delete beg end type ?_)
+            (let* ((displacement (- size-before (buffer-size)))
+                   (result (if (= p-before (point)) ;; Forward motion
+                               (if is-append (+ beg displacement) beg)
+                             ;; Backward motion
+                             (if is-append end (point)))))
+              (throw 'evil-insert-plus-quit result))))))))
 
 (defun evil-insert-plus--vcount ()
   "Calculate line count for visual line/block insertions."
   (when (and (evil-visual-state-p)
-			 (memq (evil-visual-type) '(line block)))
-	(save-excursion
-	  (let ((m (mark)))
-		(evil-visual-rotate 'upper-left)
-		(prog1 (count-lines evil-visual-beginning evil-visual-end)
-		  (set-mark m))))))
+             (memq (evil-visual-type) '(line block)))
+    (save-excursion
+      (let ((m (mark)))
+        (evil-visual-rotate 'upper-left)
+        (prog1 (count-lines evil-visual-beginning evil-visual-end)
+          (set-mark m))))))
 
 ;;;###autoload
 (evil-define-operator evil-insert-plus (beg end &optional type count)
@@ -57,19 +57,19 @@ IS-APPEND determines if the operation is an append or insert."
   :move-point nil
   (interactive "<R><c>") ; <R> for range and type, <c> for count
   (let ((vcount (evil-insert-plus--vcount)))
-	(cond
-	 ((eq type 'line)
-	  ;; visual-goto-line motions - e.g. `evil-goto-first-line'
-	  (unless (or (evil-visual-state-p)
-				  (eq evil-this-motion 'evil-line-or-visual-line))
-		(goto-char beg))
-	  (evil-insert-line count vcount))
-	 ((eq type 'block)
-	  (goto-char beg)
-	  (evil-insert count vcount))
-	 (t
-	  (goto-char (evil-insert-plus--target beg end type nil))
-	  (evil-insert 1)))))
+    (cond
+     ((eq type 'line)
+      ;; visual-goto-line motions - e.g. `evil-goto-first-line'
+      (unless (or (evil-visual-state-p)
+                  (eq evil-this-motion 'evil-line-or-visual-line))
+        (goto-char beg))
+      (evil-insert-line count vcount))
+     ((eq type 'block)
+      (goto-char beg)
+      (evil-insert count vcount))
+     (t
+      (goto-char (evil-insert-plus--target beg end type nil))
+      (evil-insert 1)))))
 
 ;;;###autoload
 (evil-define-operator evil-append-plus (beg end &optional type count)
@@ -77,24 +77,24 @@ IS-APPEND determines if the operation is an append or insert."
   :move-point nil
   (interactive "<R><c>") ; <R> for range and type, <c> for count
   (let ((vcount (evil-insert-plus--vcount)))
-	(cond
-	 ((eq type 'line)
-	  ;; visual-goto-line motions - e.g. `evil-goto-line'
-	  (unless (or (evil-visual-state-p)
-				  (eq evil-this-motion 'evil-line-or-visual-line))
-		(goto-char end))
-	  (evil-append-line count vcount))
-	 ((eq type 'block)
-	  (let* ((range (evil-visual-range))
-			 (beg-col (evil-column (car range)))
-			 (end-col (evil-column (cadr range)))
-			 (right-col (max beg-col end-col)))
-		(goto-char beg)
-		(move-to-column (1- right-col))
-		(evil-append count vcount)))
-	 (t
-	  (goto-char (evil-insert-plus--target beg end type t))
-	  (evil-insert 1)))))
+    (cond
+     ((eq type 'line)
+      ;; visual-goto-line motions - e.g. `evil-goto-line'
+      (unless (or (evil-visual-state-p)
+                  (eq evil-this-motion 'evil-line-or-visual-line))
+        (goto-char end))
+      (evil-append-line count vcount))
+     ((eq type 'block)
+      (let* ((range (evil-visual-range))
+             (beg-col (evil-column (car range)))
+             (end-col (evil-column (cadr range)))
+             (right-col (max beg-col end-col)))
+        (goto-char beg)
+        (move-to-column (1- right-col))
+        (evil-append count vcount)))
+     (t
+      (goto-char (evil-insert-plus--target beg end type t))
+      (evil-insert 1)))))
 
 ;; VIM Quirk: Make them behave similar to evil-change specially for
 ;; `evil-forward-word-begin' & `evil-forward-WORD-begin' motions
